d5ed8037d01b8372018a45490450b6406f723801
vanangamudi <selva.developer@gmail.com>
2022-07-24 15:06:16 +0530

    same thing

--- resources.py
+++
@@ -1,4 +0,0 @@
-XSV_DELIMITER = '\t'
-THARAVU_VER = 'tharavu'
-DEFAULT_DICTIONARY_FILES = [f'{THARAVU_VER}/chorkuviyal.22mar22.tsv.gz', f'{THARAVU_VER}/chorpirappiyal-agarathi.tsv.gz']
-CHELLUMAI_ADAVU_FILE = f'{THARAVU_VER}/orungurri-chellumai-cholloadai.tsv'
